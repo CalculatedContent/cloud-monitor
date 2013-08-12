425ce8f2c063f096e00e55778fa93ebffb6036e8
Charles H Martin <charlesmartin14@gmail.com>
2013-08-11 19:20:33 -0700

    .

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,10 @@
+#
+# Copyright (c) 2013 Charles H Martin, PhD
+#  
+#  Calculated Content 
+#  http://calculatedcontent.com
+#  charles@calculatedcontent.com
+#
 # pwd = File.expand_path File.dirname(__FILE__)
 # log_dir = File.expand_path "#{pwd}/../logs"
 # 
